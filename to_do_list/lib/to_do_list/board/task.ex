614a71bd09f85fddc1b1277fc2ed57a2165f1d00
WesleyVitor <wesleydemorais@outlook.com.br>
2024-05-04 14:07:33 -0300

    refactor(Task): creating changeset function

--- to_do_list/lib/to_do_list/board/task.ex
+++ to_do_list/lib/to_do_list/board/task.ex
@@ -1,6 +1,6 @@
 defmodule ToDoList.Board.Task do
   use Ecto.Schema
-
+  import Ecto.Changeset
   schema "tasks" do
     field :title, :string
     field :description, :string
@@ -9,4 +9,22 @@ defmodule ToDoList.Board.Task do
 
     timestamps()
   end
+
+  defp validate_due_date(:due_date, due_date) do
+    if due_date < Date.utc_today() do
+      [:error, "Due date must be in the future"]
+    else
+      []
+    end
+  end
+
+  def changeset(task \\ %__MODULE__{}, attrs) do
+    task
+      |> cast(attrs, [:title, :description, :due_date, :status])
+      |> validate_required([:title, :description, :due_date, :status])
+      |> validate_inclusion(:status, in: [:pending, :in_progress, :done])
+      |> validate_length(:title, min: 1, max: 100)
+      |> validate_change(:due_date, &validate_due_date/2)
+  end
+
 end
