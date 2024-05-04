2e5aa333c1343cec899713133e49a9475d960d15
WesleyVitor <wesleydemorais@outlook.com.br>
2024-05-04 11:33:47 -0300

    first commit

---
+++ to_do_list/config/test.exs
@@ -0,0 +1,29 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :to_do_list, ToDoList.Repo,
+  database: Path.expand("../to_do_list_test#{Mix.env()}.db", Path.dirname(__ENV__.file)),
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :to_do_list, ToDoListWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "dcSa8AqLEM3HVwNWKQK6k0eJP/s7ZtFFh3qQ8MI7HpZNomL1iARFSmicPcHsdIN3",
+  server: false
+
+# In test we don't send emails.
+config :to_do_list, ToDoList.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
