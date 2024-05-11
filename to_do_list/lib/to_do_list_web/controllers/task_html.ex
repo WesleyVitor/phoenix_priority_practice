05078e95f310fb6506ebd13ab5b427dca4068b5c
WesleyVitor <wesleydemorais@outlook.com.br>
2024-05-11 16:08:12 -0300

    refactor: using function component to list tasks

--- to_do_list/lib/to_do_list_web/controllers/task_html.ex
+++ to_do_list/lib/to_do_list_web/controllers/task_html.ex
@@ -2,4 +2,19 @@ defmodule ToDoListWeb.TaskHTML do
   use ToDoListWeb, :html
 
   embed_templates "task_html/*"
+
+
+  attr :tasks, :string, required: true
+  def list_all_tasks(assigns) do
+    ~H"""
+    <ul>
+      <%= for item <- @tasks do%>
+        <li> <%= item.title %> </li>
+        <li> <%= item.description %> </li>
+        <li> <%= item.due_date %> </li>
+        <li> <%= item.status %> </li>
+      <% end %>
+    </ul>
+    """
+  end
 end
